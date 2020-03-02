68f9d5c54bcfda2e86ece53969522b156d97af84
Tetsuro-11 <nishiotetsurou@nishiotetsurounoMacBook-puro.local>
2020-03-02 20:50:35 +0900

    Initial Commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,57 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only:[:show, :edit, :update, :destroy]
+
+  def index
+    @tweet = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if params[:back]
+      render :new
+    else
+      if @tweet.save
+        redirect_to tweets_path, notice:"Tweetを投稿しました。"
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Tweetを編集しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweetを削除しました。"
+  end
+
+  def confirm
+    @tweet = Tweet.new(tweet_params)
+    render :new if @tweet.invalid?
+  end
+
+  private
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+    
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
+end
